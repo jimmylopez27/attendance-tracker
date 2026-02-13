e276613e0bf87d48db8b6e4769fc652e70e47e5e
Jimmy <jbalayanan@gmail.com>
2026-02-13 12:37:46 +0700

    I added new Data

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -155,7 +155,7 @@ window.resizable(False, False)
 
 title = tk.Label(
     window,
-    text="Sunflower Trilingual School",
+    text="Patrick Lopez International School",
     font=("Arial", 16, "bold")
 )
 title.pack(pady=15)
